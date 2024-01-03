1c38553e838bd68259d410d871aa7d5319e21858
lynn pepin <lynnpepin@tutanota.com>
2024-01-02 23:56:01 -0500

    update template

--- example_draw_lines/src/main.rs
+++ example_draw_lines/src/main.rs
@@ -2,8 +2,7 @@
 use nannou::prelude::*;
 use rand::{Rng};
 const N: usize = 100;
-
-const MAX_TT: f32 = 10.0;
+const MAX_TT: f64 = 10.0;
 const MAX_NN: usize = 600;
 
 fn main() {
@@ -11,7 +10,6 @@ fn main() {
     .update(update)
     .simple_window(view)
     .size(512, 512)
-    .background_color(BLACK)
     .run();
 }
 
@@ -21,6 +19,8 @@ struct Model {
   rng: rand::rngs::ThreadRng,
   tt: f64,
   nn: usize,
+  mouse_xs: Vec<f32>,
+  mouse_ys: Vec<f32>
 }
 
 
@@ -30,7 +30,9 @@ fn model(app: &App) -> Model {
   Model {
     rng: rand::thread_rng(),
     tt: 0.0,
-    nn: 0
+    nn: 0,
+    mouse_xs: vec![0.0, 0.0],
+    mouse_ys: vec![0.0, 0.0],
   }
 }
 
@@ -43,26 +45,37 @@ fn update(app: &App, model: &mut Model, update: Update) {
   model.tt += update.since_last.secs();
   model.nn += 1;
 
-  // quit if tt > MAX_TT or nn > MAX_NN
-  /*
-  if model.tt > MAX_TT || model.nn > MAX_NN {
-    app.quit();
-  }
-  */
-}
+  model.mouse_xs = vec![app.mouse.x, model.mouse_xs[0]];
+  model.mouse_ys = vec![app.mouse.y, model.mouse_ys[0]];
 
+  // if model.tt > MAX_TT || model.nn > MAX_NN
+  // { app.quit(); }  
+}
 
  
 // Render
 fn view(app: &App, model: &Model, frame: Frame){
   let draw = app.draw();
-  draw.background().color(BLACK);
-  draw.to_frame(app, &frame).unwrap();
+
+  // draw line
+  draw.line()
+    .start(pt2(model.mouse_xs[0], model.mouse_ys[0]))
+    .end(pt2(model.mouse_xs[1], model.mouse_ys[1]))
+    .weight(3.0)
+    .color(WHITE);
+
   
+  draw.to_frame(app, &frame).unwrap();
+
   // optional: output frame to "output_{nn}.png"
   //let file_path = "output/".to_string() + &model.nn.to_string() + ".png";
   //app.main_window().capture_frame(file_path);
-
 }
 
 // eof
+
+/*
+todos
+
+
+*/
