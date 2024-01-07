a25ff49f91eeaee2140c26ab20f7ebfbd5546214
lynn pepin <lynnpepin@tutanota.com>
2024-01-06 21:05:23 -0500

    day 4 video

--- day-04-pixels/render.sh
+++ day-04-pixels/render.sh
@@ -10,8 +10,9 @@ ffmpeg -framerate 60 \
        -i output/%d.png \
        -c:v libx264 \
        -profile:v high \
-       -crf 30 \
+       -crf 18 \
        -pix_fmt yuv420p \
        output.mp4
+
 
 rm output/*.png
--- day-04-pixels/src/main.rs
+++ day-04-pixels/src/main.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code, unused_variables, unused_mut, unused_imports)]
 use nannou::prelude::*;
-use rand::{Rng};
+use rand::prelude::*;
+use std::ops::*;
 const N: usize = 100;
 
 const MAX_TT: f32 = 10.0;
@@ -17,32 +18,31 @@ fn view(app: &App, frame: Frame) {
   let draw = app.draw();
   //draw.background().color(BLACK);
 
-  let (mut ax, mut ay) = (-256.0,  -256.0);
-  let (mut bx, mut by) = ( 256.0,  256.0);
-  let (mut mid_x, mut mid_y);
-  let (    mx,     my) = (app.mouse.x, app.mouse.y);
+  let (mut ax, mut ay)       = (-256.0,      -256.0);
+  let (mut bx, mut by)       = ( 256.0,       256.0);
+  let (mut mid_x, mut mid_y) = ((ax+bx)/2.0, (ay+by)/2.0);
+  let (    mx,     my)       = (app.mouse.x, app.mouse.y);
 
   for ii in 0..7 {
-    (mid_x, mid_y) = ((ax+bx)/2.0, (ay+by)/2.0);
     if mx < mid_x { bx = mid_x; } else { ax = mid_x; }
     if my < mid_y { by = mid_y; } else { ay = mid_y; }
+    (mid_x, mid_y) = ((ax+bx)/2.0, (ay+by)/2.0);
 
     // draw mesh
     draw.quad()
       .x_y(mid_x, mid_y)
       .w_h(bx-ax, by-ay)
-      .rgba(1.0, 1.0, 1.0, 0.001);
+      .rgba(
+        // stackoverflow.com/questions/34215280
+        rand::random::<f32>().mul(2.0).round().div(2.0),
+        rand::random::<f32>().mul(2.0).round().div(2.0),
+        rand::random::<f32>().mul(2.0).round().div(2.0),
+        2.0.powf((ii - 6) as f32)
+      );
   }
-  println!("{} {}", mx, my);
-
-  /*
-  draw.quad()
-    .x_y(app.mouse.x, app.mouse.y)
-    .w_h(4.0, 4.0)
-    .color(WHITE);
-  */
   draw.to_frame(app, &frame).unwrap();
 
-  //let file_path = "output/".to_string() + &model.nn.to_string() + ".png";
-  //app.main_window().capture_frame(file_path);
+  app.main_window().capture_frame(
+    format!{"output/{}.png", app.elapsed_frames()}
+  );
 }
